953f745218b37b37e2eb7ac28fbe31577a2f383a
bruce-mig <brucetmig@icloud.com>
2025-01-12 17:33:38 +0200

    modified func name for network_convert_ip_p_to_n

--- network_utils.cpp
+++ network_utils.cpp
@@ -1,7 +1,7 @@
 /*
  * =====================================================================================
  *
- *       Filename:  network_utils.c
+ *       Filename:  network_utils.cpp
  *
  *    Description: This file contains routines to work with Network sockets programs 
  *
@@ -22,7 +22,7 @@
 #include <memory.h>
 #include "network_utils.h"
 
-char * network_convert_ip_n_to_p(uint32_t ip_addr,
+char* network_convert_ip_n_to_p(uint32_t ip_addr,
                     char *output_buffer){
 
     char *out = NULL;
@@ -35,7 +35,7 @@ char * network_convert_ip_n_to_p(uint32_t ip_addr,
     return out;
 }
 
-uint32_t network_covert_ip_p_to_n(const char *ip_addr){
+uint32_t network_convert_ip_p_to_n(const char *ip_addr){
 
     uint32_t binary_prefix = 0;
     inet_pton(AF_INET, ip_addr, &binary_prefix);
